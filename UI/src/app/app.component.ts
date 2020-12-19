ed8c4c3f7e7ad67fb42c56aa14648a1d68850f31
angular654 <v.ivahnenko@outlook.com>
2020-12-19 22:14:04 +0300

    create test UI

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileUploadService } from './file-upload.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'snake-vision';
+  link: string = "";
+  loading: boolean = false; 
+  file: any = null; 
+ 
+  constructor(private fileUploadService: FileUploadService) { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(event: any) {
+    this.file = event.target.files[0];
+  }
+
+  onUpload() {
+    this.loading = !this.loading;
+    this.fileUploadService.upload(this.file).then(
+      (res: any) => {
+        this.link = res;
+        this.loading = false;  
+      }
+    ).catch((err) => {
+      console.log(err)
+    })
+  }
 }
---
+++ UI/src/app/file-upload.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  url = "http://127.0.0.1:8000/upload"
+
+  constructor(private http: HttpClient) { }
+  upload(file: any): Promise<any> {
+    const formData = new FormData();
+    formData.append("file", file, file.name);
+    return this.http.post(this.url, formData).toPromise()
+  }
+} 
