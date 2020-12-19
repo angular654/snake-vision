0ff7c711217fdea0274e28533c3b07e851ace993
angular654 <v.ivahnenko@outlook.com>
2020-12-19 15:05:30 +0300

    create test api for uploading/downloading

--- Backend/main.py
+++ Backend/main.py
@@ -1,32 +1,44 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, Response, stream_with_context
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'files'
-ALLOWED_EXTENSIONS = set(
-    ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', '.jpg', '.png', '.mp4', '.avi'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'mp4', 'avi', 'ogg', 'MOV','WMV','FLV','AVI'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['file']
-    return file.filename + " загружен!"
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return file.filename + " uploaded!"
 
-
-@app.route('/download', methods=['GET'])
-def download_file():
-    filename = "OUT_VID.mp4"
-    return filename + " скачан!"
-
+### SEND CHUNKS
+CHUNK_SIZE = 8192
+def read_file_chunks(path):
+    with open(path, 'rb') as fd:
+        while 1:
+            buf = fd.read(CHUNK_SIZE)
+            if buf:
+                yield buf
+            else:
+                break
+@app.route('/download/<name>')
+def serve_video(name):
+    fp = os.path.join(app.config['UPLOAD_FOLDER'], name)
+    return Response(
+        stream_with_context(read_file_chunks(fp)),
+        headers={
+                'Content-Disposition': f'attachment; filename={name}'
+        }
+    )
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+  app.run(debug=True, port=8000)
